b42e7e5123c12e91d425d9259f4431cf91495e01
ETOgaosion <gaoziyuan19@mails.ucas.ac.cn>
2022-05-24 16:22:18 +0800

    modify design for SSA translation

--- src/IROperands.h
+++ src/IROperands.h
@@ -35,8 +35,16 @@ public:
     virtual MetaDataType getMetaDataType() const { return MetaDataType::VOID; };
     virtual bool getIsArray() const { return false; };
     virtual std::size_t getSize() const { return 0; };
+    virtual bool getAssigned() const { return false; };
+    virtual IROperand *getLatestVersionSymbol() const { return nullptr; };
+    virtual bool getAliasToSymbol() const { return false; };
+    virtual IROperand *getSymbolVariable() const { return nullptr; }
 
     virtual bool setValue(IRValue *inValue) { return false; };
+    virtual bool setAssigned() { return false; }
+    virtual bool setLatestVersionSymbol(IROperand *newVersionSymbol) { return false; };
+    virtual bool setAliasToSymbol() { return false; };
+    virtual bool setSymbolVariable(IROperand *inSymbolVariable) { return false; };
 
     virtual void setMemOffset(int offset) {};
     virtual int getMemOffset() const { return 0; };
@@ -77,7 +85,9 @@ public:
 class IRSymbolVariable : public IROperand {
 private:
     AbstractSymbol *symbol;
+    bool assigned;
     IRValue *curValue;
+    IROperand *latestVersionSymbol;
 
 public:
     IRSymbolVariable(AbstractSymbol *newSymbol);
@@ -86,8 +96,12 @@ public:
     MetaDataType getMetaDataType() const override { return symbol->getMetaDataType(); };
     bool getIsArray() const override { return symbol->getIsArray(); };
     std::size_t getSize() const override { return symbol->getSize(); };
+    bool getAssigned() const override { return assigned; };
+    IROperand *getLatestVersionSymbol() const override { return latestVersionSymbol; };
     
     bool setValue(IRValue *inValue) override { curValue = inValue; return true; };
+    bool setAssigned() override { assigned = true; return true; };
+    bool setLatestVersionSymbol(IROperand *latestSymbol) override { latestVersionSymbol = latestSymbol; return true; }
 
     void setMemOffset(int offset) override;
     int getMemOffset() const override;
@@ -99,7 +113,10 @@ private:
     MetaDataType metaDataType;
     bool isArray;
     std::size_t size;
+    bool assigned;
     IRValue *curValue;
+    bool aliasToSymbol;
+    IROperand *symbolVariable;
 
 public:
     IRTempVariable(std::string newName, SymbolType newSymbolType, MetaDataType newMetaDataType, bool newIsArray, std::size_t newSize);
@@ -108,8 +125,14 @@ public:
     MetaDataType getMetaDataType() const override { return metaDataType; };
     bool getIsArray() const override { return isArray; };
     std::size_t getSize() const override { return size; };
+    bool getAssigned() const override { return assigned; };
+    bool getAliasToSymbol() const override { return aliasToSymbol; };
+    IROperand *getSymbolVariable() const override { return symbolVariable; };
 
     bool setValue(IRValue *inValue) override { curValue = inValue; return true; };
+    bool setAssigned() override { assigned = true; return true; };
+    bool setAliasToSymbol() override { aliasToSymbol = true; return true; };
+    bool setSymbolVariable(IROperand *inSymbolVariable) override { aliasToSymbol = true; symbolVariable = inSymbolVariable; return true; };
 
     void setMemOffset(int offset) override;
     int getMemOffset() const override;
